836353705084de34bf716174c1c4753cf882da83
Misha <mishakl10@ukr.net>
2021-04-10 17:24:37 +0300

    Custom hook for fetching Listings

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { server } from '../../lib/api';
+import React from 'react'
+import { server, useQuery } from '../../lib/api';
 import { ListingsData, DeleteListingData, DeleteListingVariables, Listing } from './types';
 
 const LISTINGS = `
@@ -31,12 +31,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const [listings, setListings] = useState<Listing[] | null>(null)
-
-  const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS })
-    setListings(data.listings)
-  }
+  const { data } = useQuery<ListingsData>(LISTINGS)
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>(
@@ -45,8 +40,9 @@ export const Listings = ({ title }: Props) => {
           variables: { id }
         }
       )
-    fetchListings()
   }
+
+  const listings = data ? data.listings : null
 
   const listingsList = (
     <ul>
@@ -65,7 +61,6 @@ export const Listings = ({ title }: Props) => {
     <div>
       <h2>{title}</h2>
       {listingsList}
-      <button onClick={fetchListings}>Query</button>
     </div>
   )
 }
