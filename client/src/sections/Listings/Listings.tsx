ac949d52877836aafa818d5fbc2a0fc944c93024
Misha <mishakl10@ukr.net>
2021-04-10 16:48:01 +0300

    Delete listing from client

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { server } from '../../lib/api';
-import { ListingsData } from './types';
+import { ListingsData, DeleteListingData, DeleteListingVariables } from './types';
 
 const LISTINGS = `
   query Listings {
@@ -18,6 +18,14 @@ const LISTINGS = `
   }
 `
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`
+
 interface Props {
   title: string
 }
@@ -29,10 +37,16 @@ export const Listings = ({ title }: Props) => {
     console.log(data);
   }
 
+  const deleteListing = async () => {
+    const { data } = await server.fetch<DeleteListingData, DeleteListingVariables>({ query: DELETE_LISTING, variables: { id: '60718a680c76d927ac94a635' } })
+    console.log(data);
+  }
+
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query</button>
+      <button onClick={deleteListing}>Delete</button>
     </div>
   )
 }
