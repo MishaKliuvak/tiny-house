146291d5b48b48efe894771681494fb18b92dfa5
Misha <mishakl10@ukr.net>
2021-04-11 12:19:41 +0300

    Routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Listings } from './sections'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Listings, Home, Host, Listing, NotFound, User } from './sections'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import './styles/index.css'
@@ -9,9 +10,24 @@ const client = new ApolloClient({
   uri: '/api'
 })
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/host" component={Host} exact />
+        <Route path="/listing/:id" component={Listing} exact />
+        <Route path="/listings/:location?" component={Listings} exact />
+        <Route path="/user/:id" component={User} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client}>
-    <Listings title='Tiny House'/>
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
